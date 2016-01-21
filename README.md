c51840663a93b29fc44cf4c4410dfbfc93d2dc1a
Kevin Smets <kevin.smets2@kbc.be>
2016-01-21 20:37:31 +0100

    v0.2.3 - Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# dep-rep
+
+Dependency reporter and manager for bower and npm
+
+## Get started
+
+### CLI usage
+
+    npm i -g dep-rep
+    
+From then on, you can run `dep-rep` in any folder containing a `package.json`.
+
+For `bower` you need to run `dep-rep -b` or `dep-rep --bower`.
+
+### Node usage
+
+    npm i dep-rep    
+
+```
+const depRep = require('dep-rep');
+
+depRep.report(currentJsonAsString, oldJsonAsString)
+```
+
+## Contribute
+
+### Run CI
+
+    npm run ci
+    
+### Generate coverage
+  
+    npm run coverage
---
+++ test/specs/depRepSpec.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+'use strict';
+
+const assert = require('assert');
+
+const depRep = require('../../lib/depRep');
+
+const oldJson = require('../fixtures/old.json');
+const newJson = require('../fixtures/new.json');
+
+function key(number, dev) {
+    var prefix = "/dependencies/";
+
+    if (dev) prefix = "/devDependencies/";
+
+    return prefix + number;
+}
+
+describe('Compare', function () {
+    describe('#report()', function () {
+        it('should generate a proper report for dependencies', function () {
+            depRep
+                .report(oldJson, newJson)
+                .then(function () {
+                    assert.equal(report[key(1)].status, "major");
+                    assert.equal(report[key(2)].status, null);
+                    assert.equal(report[key(3)], null);
+                    assert.equal(report[key(4)].status, null);
+                    assert.equal(report[key(5)].status, null);
+                    assert.equal(report[key(6)], null);
+                    assert.equal(report[key(7)].status, null);
+                    assert.equal(report[key(8)].status, "minor");
+                    assert.equal(report[key(9)].status, "major");
+                    done();
+                });
+        });
+    });
+
+    describe('#report()', function () {
+        it('should generate a proper report for devDependencies', function () {
+            depRep
+                .report(oldJson, newJson)
+                .then(function () {
+                    assert.equal(report[key(1, true)].status, "major");
+                    assert.equal(report[key(2, true)].status, null);
+                    assert.equal(report[key(3, true)], null);
+                    assert.equal(report[key(4, true)].status, null);
+                    assert.equal(report[key(5, true)].status, null);
+                    assert.equal(report[key(6, true)], null);
+                    assert.equal(report[key(7, true)].status, null);
+                    assert.equal(report[key(8, true)].status, "minor");
+                    assert.equal(report[key(9, true)].status, "major");
+                    done();
+                });
+        });
+    });
+});
